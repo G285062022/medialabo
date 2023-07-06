c21c33adb4948aaf4bf004f6b121fb9e165bed49
G285062022 <228506@st.takushoku-u.ac.jp>
2023-07-06 16:44:13 +0900

    課題

--- kazuate.js
+++ kazuate.js
@@ -3,44 +3,54 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
+let kaisu = 1;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let b = document.querySelector( '#answer');
+b.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  // 課題3-1: 正解判定する
+  
+  // 課題3-1: 正解判定する  
+  let i = document.querySelector('input[name="yoso"]');
+  let yoso = i.value; 
+
+  let SpanKaisu = document.querySelector('span#kaisu');
+  SpanKaisu.textContent = kaisu;
+
+  let SpanAnswer = document.querySelector('span#answer');
+  SpanAnswer.textContent = yoso;
+
+  let SpanResult = document.querySelector('p#result');
+
+  if(kaisu < 3){
+    if(hantei === kotae){
+      SpanResult.textContent = 'せいかーい。おめでとう!';
+    }else{
+      if(hantei<kotae){
+        SpanResult.textContent = 'ザンネーン、答えはもっと大きいです。';
+      }else{
+        SpanResult.textContent = 'ザンネーン、答えはもっと小さいです。';
+      }
+    }
+  }else if(kaisu === 3){
+    if(hantei === kotae){
+      SpanResult.textContent = 'せいかーい。おめでとう!';
+    }else{
+      if(hantei<kotae){
+        SpanResult.textContent = 'ザンネーン! 答えは'+kotae+'でした~';
+      }else{
+        SpanResult.textContent = 'ザンネーン! 答えは'+kotae+'でした~';
+      }
+    }
+    }else if(kaisu > 3){
+      SpanResult.textContent = 'もうゲームは終わってるよ。答えは'+kotae+'でした~';
+    }
+    kaisu = kaisu + 1;
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-  let yoso = 4;
-  kaisu += 1;
-
-  console.log(kaisu + "回目の予想: " + yoso)
-
-  if (kaisu > 3) {
-    console.log("答えは" + kotae +"でした．すでにゲームは終わっています");
-  }
-  else if(kaisu == 3){
-    console.log("まちがい. 残念でした答えは" + kotae + "です.")
-  }
-  else{
-    if (yoso > kotae) {
-        console.log("まちがい.答えはもっと小さいですよ");
-      }
-    
-    else if(yoso < kotae) {
-        console.log("まちがい.答えはもっと大きいですよ");
-      }
-    
-    else{
-        console.log("正解です.おめでとう!");
-      }
-  }
 }
