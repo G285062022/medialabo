84666638bb4ff8d52789e143c240a2bd19d9893a
G285062022 <228506@st.takushoku-u.ac.jp>
2023-07-20 16:53:14 +0900

    課題

--- weather.js
+++ weather.js
@@ -2,9 +2,9 @@ let searchBtn = document.getElementById('search-btn');
 searchBtn.addEventListener('click', iroiro);
 
 function iroiro() {
-  let cityId = document.getElementById('city-id').value.trim(); 
+  let cityId = document.getElementById('city-id').value.trim();
   if (cityId === '') {
-    console.log('都市のIDを入力してください'); 
+    console.log('都市のIDを入力してください');
     return;
   }
 
@@ -16,4 +16,47 @@ function iroiro() {
     .then(finish);
 }
 
+function showResult(resp) {
+  let data = resp.data;
+  if (typeof data === 'string') {
+    data = JSON.parse(data);
+  }
+  console.log(data);
 
+  // Display weather information
+  let p1 = document.querySelector('p#name');
+  p1.textContent = "都市名: " + data.name;
+
+  let p2 = document.querySelector('p#temp_max');
+  p2.textContent = "最高気温: " + data.main.temp_max + "℃ ";
+
+  let p3 = document.querySelector('p#temp_min');
+  p3.textContent = "最低気温: " + data.main.temp_min + "℃";
+
+  let p4 = document.querySelector('p#situ');
+  p4.textContent = "湿度: " + data.main.humidity + "% ";
+
+ 
+  let p5 = document.querySelector('p#lat');
+  p5.textContent = "緯度: " + data.coord.lat;
+
+  let p6 = document.querySelector('p#lon');
+  p6.textContent = "経度: " + data.coord.lon;
+
+  let p7 = document.querySelector('p#weather');
+  p7.textContent = "天気: " + data.weather[0].description;
+
+  let p8 = document.querySelector('p#wind_speed');
+  p8.textContent = "風速: " + data.wind.speed + " m/s";
+
+  let p9 = document.querySelector('p#wind_deg');
+  p9.textContent = "風向: " + data.wind.deg + "°";
+}
+
+function showError(err) {
+  console.log(err);
+}
+
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
